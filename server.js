cdec7a4e78455e3638a96ef317dde31fa86ac5c8
msp4msps <msp4msps@tminus365.com>
2021-11-06 12:54:26 -0600

    update file path

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 
-app.use(express.static("public"));
+app.use(express.static("./Develop/public"));
 
 // GET Route for homepage
 app.get("/", (req, res) =>
